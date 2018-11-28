63c9f15d553bb17cef5c67b610af73f1eaf16fd1
Josh-WBS <josh.wood@wbs.uk.com>
2018-11-28 13:28:09 +0000

    Add files via upload

---
+++ JavaFXController.java
@@ -0,0 +1,13 @@
+package dev.subbotted.javafx.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class JavaFXController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {}
+	
+}
